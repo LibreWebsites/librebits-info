64b35df852fa3db49616490696920fea3f76b344
root <root@i-ching.localhost>
2016-01-31 23:08:13 +0000

    +- navbar

--- components/php/navbar.php
+++ components/php/navbar.php
@@ -2,6 +2,7 @@
       <div class="container">
 
         <nav class="navbar navbar-inverse navbar-static-top">
+	 <div class="container">
           <div class="container">
             <div class="navbar-header">
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -14,9 +15,8 @@
             </div>
             <div id="navbar" class="navbar-collapse collapse">
               <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php">Home</a></li>
-<!--                <li><a href="#about">About</a></li> -->
                 <li><a href="https://jordila.librebits.info">Blog</a></li>
+                <li><a href="pay.php"> €nergiza </a></li>
                 <li><a href="contact.php">Contacta</a></li>
 <!--                <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
@@ -33,6 +33,7 @@
               </ul>
             </div>
           </div>
+	 </div>
         </nav>
 
       </div>
