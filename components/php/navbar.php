c18abd06b563224eae970078e4728cb45c230396
root <root@i-ching.localhost>
2016-01-31 12:46:07 +0000

    ++ botón Blog menu --> jordila.librebits.info

--- components/php/navbar.php
+++ components/php/navbar.php
@@ -16,6 +16,7 @@
               <ul class="nav navbar-nav">
                 <li class="active"><a href="index.php">Home</a></li>
 <!--                <li><a href="#about">About</a></li> -->
+                <li><a href="https://jordila.librebits.info">Blog</a></li>
                 <li><a href="contact.php">Contacta</a></li>
 <!--                <li class="dropdown">
                   <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
