9611c29ebe4cc6e60f14328d89b3326127667e60
root <root@i-ching.localhost>
2016-02-08 12:02:06 +0000

    ++ herramientas html ++css | logos/... ?

---
+++ herramientas.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+
+    <!--- here goes meta.php -->
+    <?php include "components/php/meta.php"; ?>
+    
+    <title>Librebits | Herramientas</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="Carousel%20Template%20for%20Bootstrap_files/bootstrap.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    
+    
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/ie-emulation-modes-warning.js">
+    </script>
+    
+    <style>.carbonad,
+    #content > #right > .dose > .dosesingle,
+    #content > #center > .dose > .dosesingle,
+    #carbonads-container
+    {display:none !important;}
+
+    body {
+    padding-top: 100px;
+    }
+    .starter-template {
+    padding: 40px 15px;
+    text-align: center;
+    }
+
+    </style>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="Carousel%20Template%20for%20Bootstrap_files/carousel.css" rel="stylesheet">
+ </head>
+
+<!-- NAVBAR
+================================================== -->
+  <body>
+
+
+    <!-- { navbar-->
+    <?php include "components/php/navbar.php"; ?>
+
+
+
+<!-- PayPal button  Container 
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+
+    <div class="container">
+
+
+      <section id="weapons" class="row">
+	<h2 class="orange">Nuestras herramientas</h2>
+	<h4 class="display">Nos empoderan.</h4><img src="logos/GNUlinux.svg.png" alt="GNU/Linux" class="tux">
+	<!--    <h4 class="display">We praise them. They empower us.</h4><img src="static/images/tux.svg" alt="TUX" class="tux"> -->
+	<ul class="weapons-logos">
+
+	  <li>
+	    <!-- obviar primera columna                 <a href="https://www.android.com/" target="_blank"><img class="display" src="assets/images/Android.svg.pn\
+		 g" alt="Android" title="Android" border="0"></a> -->
+	  </li>
+
+	  <li>
+	    <a href="https://drupal.org/" target="_blank"><img class="display" src="logos/drupal.svg.png" alt="Drupal CMS" title="Drupal" border=\
+							       "0"></a>
+	  </li>
+
+	  <li>
+	    <a href="http://wordpress.org/" target="_blank"><img class="display" src="logos/wordpress.svg" alt="Wordpress" title="Wordpress" bord\
+								 er="0"></a>
+	  </li>
+
+	  <li>
+	    <a href="http://moodle.org" target="_blank"><img class="display" src="logos/moodle.png" alt="Moodle LMS" title="Moodle" border="0"></\
+																		  a>
+</li>
+
+</section>
+
+</container>
+
+    
+
+    <!-- TransferWise option >> transferwise.com
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+
+  <div class="container">
+
+      <div class="row">
+	<div class="col-sm-9">
+	  <h4> Pagos Online a Librebits.info via TransferWise</h4>
+	  <div class="row">
+	    <div class="col-xs-8 col-sm-6">
+	      <p> Energizano$ con dinero en otras monedas con el cambio real via TransferWise, ahorrando en comisiones: </p>
+	    </div>
+	    <div class="col-xs-4 col-sm-6">
+	            <div class="row text-center">
+		      <p><a href="https://transferwise.com"> <img alt="TransferWise logo" src="assets/images/Transferwise_logo.png" ></a></p>
+		    </div>
+	    </div>
+	  </div>
+
+	  <!-- aqui viene la nested row para PayPal -->
+	  <div class="row">
+	    <div class="col-xs-8 col-sm-6">
+	      <p></p>
+	    </div>
+	    <div class="col-xs-4 col-sm-6">
+	            <div class="row text-center">
+		      <p></p>
+		    </div>
+	    </div>
+	  </div>
+	  
+	</div>
+	</div>
+
+  </div>
+
+
+</div><!-- /.container -->
+
+  
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/jquery.js"></script>
+    <script src="Carousel%20Template%20for%20Bootstrap_files/bootstrap.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/holder.html"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
+  
+
+</body></html>
