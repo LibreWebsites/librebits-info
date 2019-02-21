c51dec307473ad4f3d1a17200dcfd859daac5d17
Jordi <jordila@librebits.info>
2019-02-21 14:19:06 +0100

    ++ demo-chat.php , usando (en vez de jorweb@ ) JID de Marcel como destinatario de los mensajes ?

---
+++ demo-chat.php
@@ -0,0 +1,344 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <!--- here goes meta.php -->
+    <?php include "components/php/meta.php"; ?> 
+
+    <title>Librebits | Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="Carousel%20Template%20for%20Bootstrap_files/bootstrap.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/ie-emulation-modes-warning.js"></script>
+
+    <style>.carbonad,
+#content > #right > .dose > .dosesingle,
+#content > #center > .dose > .dosesingle,
+#carbonads-container
+{display:none !important;}</style>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="Carousel%20Template%20for%20Bootstrap_files/carousel.css" rel="stylesheet">
+
+
+    <!-- ConverseJS XMPP - WebClient -->
+    <!-- à la  https://conversejs.org/docs/html/quickstart.html -->
+
+    <link rel="stylesheet" type="text/css" media="screen" href="https://cdn.conversejs.org/css/converse.min.css">
+    <script src="https://cdn.conversejs.org/dist/converse.min.js"></script>
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+
+    <!-- { navbar-->
+    <?php include "components/php/navbar.php"; ?>
+
+        
+
+    <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+        <li class="" data-target="#myCarousel" data-slide-to="1"></li>
+        <li class="active" data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="item">
+          <img class="first-slide" src="assets/images/slideshow/Librebits_v4.png" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <p> <code> -. Lao-Tzu , filósofo chino ( 604 a.C. - 531 a.C. )</code> </p>
+	                   <p><h3>"Producción sin posesión,  acción sin ambición, desarrollo sin dominio"</h3></p>
+<!--              <p><a class="btn btn-lg btn-primary" href="#" role="button">Saber más >>></a></p> -->
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="first-slide" src="assets/images/slideshow/amasando-pan_dyne-org-small.jpg" alt="First slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1><code>Artesanía EnLínea</code></h1>
+              <p> En <code>librebits</code> te inspiramos en el aprendizaje, codesarrollo y adopción de las herramientas y tecnologías  que mejoren tu experiencia online.
+	      </p>
+<!--              <p><a class="btn btn-lg btn-primary" href="#" role="button">Saber más >>></a></p> -->
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          <img class="second-slide" src="assets/images/slideshow/matrix-keyboard-ii.png" alt="Second slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Hecho con nuestras manos.</h1>
+	                    <h1><code>Hecho con el corazón</code>.</h1>
+              <p>Software, Redes, Sistemas, Colaboración / Comercio-e, Datos en tiempo real...y más!. <code>Librebits</code>.</p>
+		<!--              <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p> -->
+            </div>
+          </div>
+        </div>
+        <div class="item active">
+          <img class="third-slide" src="assets/images/slideshow/colibri-wide.jpg" alt="Third slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Desarrollamos con licencias de <code>software libre</code></h1>
+              <p>Vamos más allá de las ataduras del software propietario...Cuando quieras, simplemente cambias de colaborador, pero no de solución técnica.</p>
+<!--              <p><a class="btn btn-lg btn-primary" href="#" role="button">( Adiós al "vendor-locking")</a></p> -->
+            </div>
+          </div>
+        </div>
+
+      <!-- más -->
+        <div class="item">
+          <img class="fourth-slide" src="assets/images/slideshow/acueducto.jpg" alt="Fourth slide">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1> <code> Tejemos una red cooperativa</code></h1>
+	                   <p><h3> Ágil y resiliente. Generadora de procomún.</h3></p>
+<!--              <p><a class="btn btn-lg btn-primary" href="#" role="button">Saber más >>></a></p> -->
+            </div>
+          </div>
+        </div>      
+      </div>
+      
+      <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div><!-- /.carousel -->
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+    
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+	  <span style="font-size:2.5em;" class="glyphicon glyphicon-home"></span>
+
+	  <span style="font-size:4em;" class="glyphicon glyphicon-globe"></span>
+        
+	  <span style="font-size:2.5em;" class="glyphicon glyphicon-info-sign"></span>
+
+	  <h2>Desarrollo Web</h2>
+          <h4></h4>
+	  <p>Somos tus sastres. Hacemos tu ropa a medida. Para que te sientas cómod@ en Internet. Adaptamos los contenidos publicados como un guante a tu móvil, tableta y PC. Y los enlazamos con las redes sociales desde un espacio web propio.</p><p> Aspiramos, con el tiempo, <i> a ser tus socios tecnológicos.</i></p>
+<!--          <p><a class="btn btn-default" href="http://latinamericajourneys.guru" role="button">Visit Website »</a></p> -->
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+  	  <span style="font-size:2.5em;" class="glyphicon glyphicon-cloud"></span>
+	  
+          <span style="font-size:4em;" class="glyphicon glyphicon-globe"></span>
+	  
+	  <span style="font-size:2.5em;"  class="glyphicon glyphicon-file"></span>
+          <h2>Sistemas, Seguridad y migraciones a software libre</h2>
+          <h4></h4>
+	  <p>Desarrollamos contigo sistemas, Web y herramientas de colaboración online seguras para hacer más segura fluida y ágil tu actividad, usamos derivados de Linux.</p>
+	  <p>Te acompañamos en procesos de <a href="migra.php"> migración a sistemas operativos confiables y resilientes »</a></p>
+          <p><a class="btn btn-default btn-primary" href="migra.php" role="button">Migramos ? »</a></p>
+
+<!--	             <p><a class="btn btn-lg btn-primary" href="contact.php" role="button"> Encontrar sinergias >></a></p>	  -->
+	  
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+	  <span class="glyphicons glyphicons-roundabout"></span>
+
+	  <span style="font-size:2.5em;" class="glyphicon glyphicon-user"></span>
+	  
+	  <span style="font-size:4em;" class="glyphicon glyphicon-globe"></span>
+
+	  <span style="font-size:2.5em;" class="glyphicon glyphicon-shopping-cart"></span>
+          
+          <h2>Asesoría de servicios online</h2>
+          <p> Te guiamos en ámbitos como la seguridad informática, comercio electrónico, medios de pago, conectividad, datos y visualiazción en tiempo real, nuevas tecnologías en general y cualquier reto digital que se nos pueda presentar en particular. Potenciamos tuproyecto.</p>
+	  <p></p>
+<!--          <p><a class="btn btn-default" href="#" role="button">Saber más »</a></p> -->
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+      
+
+      <!-- START THE FEATURETTES -->
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading"> ¿Comercio/pago electrónico?<span></h2>
+
+	   <span class="text-muted">Potenciamos las posibilidades de tu espacio web.</span></h2
+	  <p class="lead">  ¿tu actividad/negocio requiere de pagos más ágiles / on-line?¿integración <i>PayPal</i> u otras pasarelas de pago?. Los desarrollamos de forma segura, sencilla y eficaz. Siempre a salvo de fisgones online... </p>
+
+	  <p> También estamos experimentando con la tecnología <i>blockchain</i> y su abanico de aplicaciones como <i>criptomonedas</i>,etc <a href="contact.php">y bla,bla...¿Mejor nos escribimos y seguimos charlando »</a>:</p>
+           <p><a class="btn btn-lg btn-primary" href="contact.php" role="button"> Encontrar sinergias >></a></p>	  
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="assets/images/acrobata-a-caballo.png" alt="Generic placeholder image">
+        </div>
+      </div>
+
+
+      
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading"> El cielo y las nubes, nuestro único límite...  <span class="text-muted"> ¿Buscas poner en marcha otras herramientas/servicios en la web?</span></h2>
+          <p class="lead">Potenciamos tu proyecto. .. Lo  mencionado hasta  aquí no es más que un reflejo de lo que venimos haciendo últimamente, ... siempre podremos hacernos unas alas nuevas para dar rienda suelta a nuestra creatividad, concretándola de forma práctica y eficiente en la Web. Tu marcas el rumbo. A continuación algunas (entre otras) cosas que desarrollamos en estos momentos... </p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="assets/images/bicicleta-voladora.png" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">Mmh..¿colaboración online?</<span class="text-muted"> Trabaja en equipo, dispón y comparte tus documentos y ficheros (fotos/audio/video, etc...) de forma ubicua.</span></h2><p class="lead"> Solemos tener nuestros datos e interacciones (e-mails, etc...) repartidos entre diferentes dispositivos, servidores o soluciones poco confiables y productivas. ¿Una única capa de acceso para todos nuestros ficheros que antes se encontraban diseminados en varios dispositivos. Así podemos acceder a todo ello desde cualquier lugar en cualquier con momento? Sencillamente.</p>
+	   <p>Todo de la forma segura y privada que confiere la encriptación. Si lo prefieres implantamos tu <i>la colaboración online »</i> y herramientas de colaboración en tus propios PCs / Servidores.</p>					   
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="assets/images/acrobata-a-caballo.png" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Maestros-aprendices<span class="text-muted"> Exploradores. Buscadores... </span></h2>
+          <p class="lead"> Nos apasionan los procesos de  enseñanza - aprendizaje colectivos.
+	    De hecho... nosotros mismos ofrecemos cursos a medida, relacionados con cualesquiera de los mencionados servicios y  <a href="herramientas.php" role="button">herramientas  tecnológicas »</a> subyacentes. Todos tenemos algo que aprender. Todos tenemos algo que enseñar..</p>
+	  <p class="lead">
+	    Disfrutamos también compartiendo y aprendiendo con nuestros niños y niñas o con cualquiera con espíritu  <a href="http://hackerkids.space" role="button">HackerKid(s) »</a>.</p>
+	  <p><a class="btn btn-lg btn-primary" href="contact.php" role="button"> Proyecto HackerKids >></a></p>	  
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="assets/images/acrobata.png" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+
+            <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">¿llegó la hora de actualizar/migrar </<span class="text-muted"> tus sistemas/servidores/PCs ?, hazlo con todas las garantías.</span></h2><p class="lead"> Con una visión de conjunto, tus herramientas  diseminadas en varios dispositivos, son capaces de orquestarse con más armonía. Migra/actualiza/libera tus sistemas al software más potente actualmente. Sencillamente.</p>
+	   <p>En un proceso clave en las herramientas que te acompañan día,día... preguntanos como mejoramos en comodidad y productividad. <i>Sin ataduras »</i>  <a href="libera-tu-pc.php">Empodera tus PCs / Servidores y multiplica sus posibilidades.</a> Que trabajen por tí. No al revés.</p>					   
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="assets/images/acrobata-a-caballo.png" alt="Generic placeholder image">
+        </div>
+      </div>
+
+      <hr class="featurette-divider">
+
+
+
+      <!-- HackerKids -->
+
+
+
+
+      <div class="row">
+	<div class="col-md-12 padding-0">
+	  <img src="assets/images/slideshow/hackerkids.png" />
+	</div>
+      </div> 
+
+
+      
+      <!-- End HackerKids : test full width bottom image -->
+              <p>Espacio (educativo) de aprendizaje colectivo y autónomo para infancia y adolescencia.</p>
+              <p><a class="btn btn-lg btn-primary" href="http://hackerkids.space" role="button">Ir a HackerKids.space >></a></p>
+
+            
+      <!-- /END THE FEATURETTES -->
+
+
+      <!-- FOOTER -->
+
+      
+    <!--- here goes meta.php -->
+    <?php include "components/php/footer.php"; ?> 
+
+    <!-- /END del FOOTER -->
+      
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/jquery.js"></script>
+    <script src="Carousel%20Template%20for%20Bootstrap_files/bootstrap.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/holder.html"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="Carousel%20Template%20for%20Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
+  
+
+ </body>
+
+    <!-- ConverseJS XMPP - WebClient  SCript -->
+  <script>
+
+// auto iniciar chat con jla@ .. à la https://github.com/conversejs/converse.js/issues/679
+            // https://stackoverflow.com/questions/47034244/converse-opening-a-chat-from-api
+            // https://jsfiddle.net/jcbrand/5juvrL3c/
+    // Declaramos (primeramente!) nuestro Plugin ConverseJS , según exije la nueva API > v3.0 https://m.conversejs.org/docs/html/developer_api.html#send
+
+      converse.plugins.add('startChat', {
+              initialize: function() {
+                  var _converse = this._converse;
+                  Promise.all([
+                      _converse.api.waitUntil('rosterContactsFetched'),
+                      _converse.api.waitUntil('chatBoxesFetched')
+                  ]).then(function() {
+                      // Note, JID below must be in your contacts roster!
+                      _converse.api.chats.open('marcel@librebits.info');
+                  });
+              } 
+          });
+  
+
+    // Inicializamos ConverseJS de forma clásica
+      
+    converse.initialize({
+            authentication: 'login', 
+            auto_login: true,
+            jid: 'jorweb@librebits.info',
+            password: 'jorweb',           
+            bosh_service_url: 'https://librebits.info:5281/http-bind/',
+            // bosh_service_url: 'https://conversejs.org/http-bind/',
+            // Please use this connection manager only for testing purposes
+            // auto chat tras autologin ? :
+            // Ya Declardo (ver anterior párrafo de código!) nuestro Plugin ConverseJS según nueva API,
+            // lo ++ 'whitelist'
+            whitelisted_plugins: ['startChat'],
+            // https://conversejs.org/docs/html/configuration.html?highlight=message_archiving#message-archiving para evitar solapamiento de conversaciones de diferentes usuarios "
+            message_archiving: 'never',
+            //  cerramos la ventanita 'ControlBox' para no mostrar ni salas, ni roaster... ni ná, como la Invisibilizamos/eliminamos del todo [..]
+            show_controlbox_by_default: false
+                        
+    });
+    </script>
+
+</html>
